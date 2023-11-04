6da37101977b97966ead52a335f9db7d2c2aaeeb
ecomeel <ecomeel@gmail.com>
2023-11-04 14:19:52 +0300

    added delete post button

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -6,6 +6,7 @@ export default function Post({ post }) {
             { post.time && <p className="post__time">{post.time}</p> }
             <h3 className="post__title">{post.title}</h3>
             <p className="post__text">{post.body}</p>
+            <button className='post__delete-post-button'>Удалить пост</button>
         </li>
     )
 }
