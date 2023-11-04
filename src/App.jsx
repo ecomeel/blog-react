b025df035105988baa4480a4231851ce2b1d2379
ecomeel <ecomeel@gmail.com>
2023-11-04 11:56:58 +0300

    get posts from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 import NewPost from './components/NewPost/NewPost'
 import PostsFeed from './components/PostsFeed/PostsFeed'
-
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => console.log(posts))
+  }, []);
 
   return (
     <div className="app">
--- src/components/PostsFeed/PostsFeed.jsx
+++ src/components/PostsFeed/PostsFeed.jsx
@@ -4,6 +4,8 @@ export default function PostsFeed() {
     return (
         <div className="posts-feed">
             <Header text='Лента' />
+            <ul className="posts-feed__list">
+            </ul>
         </div>
     )
 }
