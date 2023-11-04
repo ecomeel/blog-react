dfa841a06e73307f00dd3c3f7524a3955964c0ad
ecomeel <ecomeel@gmail.com>
2023-11-04 11:45:55 +0300

    new post layout

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,9 +1,20 @@
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+import "./NewPost.css";
 
 export default function NewPost() {
     return (
         <div className="new-post">
-            <Header text='Новый пост' />
+            <Header text="Новый пост" />
+            <input
+                className="new-post__title"
+                type="text"
+                placeholder="Заголовок"
+            />
+            <textarea
+                className="new-post__text"
+                placeholder="Напишите техт"
+            ></textarea>
+            <button className="new-post__post-button">Опубликовать</button>
         </div>
-    )
+    );
 }
